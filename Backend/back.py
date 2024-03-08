be1aca15473a79524278ce7c83cc89ec09123563
Jacky Wu <jacky02wu@gmail.com>
2024-03-08 12:41:13 -0600

    jacky testing

--- Backend/back.py
+++ Backend/back.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from pymongo import MongoClient
 from flask_cors import CORS
+import datetime
 
+x = datetime.datetime.now()
+
+# Initializing flask app
 app = Flask(__name__)
 CORS(app)
 
@@ -11,25 +15,24 @@ db = client['user_database']
 collection = db['users']
 
 
-@app.route('/') # Serves the landing page 
+@app.route('/', methods=["GET"])
 def index():
     return render_template('index.html')
 
-# Handles the submission of user data from the frontend application and saves it in the database.
-@app.route('/save_user', methods=['POST']) 
+@app.route('/save_user', methods=['POST'])
 def save_user():
     if request.method == 'POST':
+        Name = request.form['Name']
         userID = request.form['userID']
         password = request.form['password']
 
-        # Check if the user already exists based on userID
+        # Check if the user already exists
         if collection.find_one({'userID': userID}):
             return 'User successfully signed-in'
-        
         else:
             # Insert the new user
-            collection.insert_one({'userID': userID, 'password': password})
+            collection.insert_one({'Name': Name, 'userID': userID, 'password': password})
             return 'New User successfully created'
-        
+
 if __name__ == '__main__':
     app.run(debug=True)
